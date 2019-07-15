0d12cd0feff639fedeb68ba4ec5b629353581437
jmbargallo <jmbargallo@gmail.com>
2019-07-15 09:13:48 +0200

    segona versio amb condicional mes elegant

--- fase2.java
+++ fase2.java
@@ -18,43 +18,29 @@ public class llista {
         name.add('7');
 
 
+        for (int i = 0; i < name.size(); i++) {
+            System.out.println(name.get(i));
+            if ((name.get(i).equals('a')) || (name.get(i).equals('e'))
+                    || (name.get(i).equals('i')) || (name.get(i).equals('o'))
+                    || (name.get(i).equals('u'))) {
 
+                System.out.println("es una vocal");
+            } else if
+            ((name.get(i).equals('0')) || (name.get(i).equals('1'))
+                            || (name.get(i).equals('2')) || (name.get(i).equals('3'))
+                            || (name.get(i).equals('4')) || (name.get(i).equals('5')
+                            || (name.get(i).equals('6')) || (name.get(i).equals('7'))
+                            || (name.get(i).equals('8')) || (name.get(i).equals('9')))) {
 
-            for (int i = 0; i < name.size(); i++) {
-                System.out.println(name.get(i));
-                if (name.get(i).equals('a')) {
-                    System.out.println("es una vocal");
-                } else if (name.get(i).equals('e')) {
-                    System.out.println("es una vocal");
-                } else if (name.get(i).equals('i')) {
-                    System.out.println("es una vocal");
-                } else if (name.get(i).equals('o')) {
-                    System.out.println("es una vocal");
-                } else if (name.get(i).equals('u')) {
-                    System.out.println("es una vocal");
-                } else if (name.get(i).equals('1')) {
-                    System.out.println("Els noms de persones no tenen numeros");
-                } else if (name.get(i).equals('1')) {
-                    System.out.println("Els noms de persones no tenen numeros");
-                } else if (name.get(i).equals('2')) {
-                    System.out.println("Els noms de persones no tenen numeros");
-                } else if (name.get(i).equals('3')) {
-                    System.out.println("Els noms de persones no tenen numeros");
-                } else if (name.get(i).equals('5')) {
-                    System.out.println("Els noms de persones no tenen numeros");
-                } else if (name.get(i).equals('6')) {
-                    System.out.println("Els noms de persones no tenen numeros");
-                } else if (name.get(i).equals('7')) {
-                    System.out.println("Els noms de persones no tenen numeros");
-                } else if (name.get(i).equals('8')) {
-                    System.out.println("Els noms de persones no tenen numeros");
-                } else if (name.get(i).equals('9')) {
-                    System.out.println("Els noms de persones no tenen numeros");
-                } else
-                    System.out.println("es una consonante");
-                ;
+                System.out.println("els noms de persones no contenen numeros!!");
+            } else if ((name.get(i).equals(' '))) {
 
-
+                System.out.println("es un espai");
+            } else {
+                System.out.println("es una consonant");
+                {
+                }
             }
         }
     }
+}
